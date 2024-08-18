1b55d4891d819905fb501ecd2b63661db4b1155f
Daniel Orosco <daniel.orosco.sc2@hotmail.com>
2024-08-17 20:21:03 -0500

    FireBase Impl

---
+++ js/config.js
@@ -0,0 +1,67 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import {
+  getStorage,
+  ref,
+  listAll,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCG-wk1XuYnss2PNy81G2VQUL373MnX3E0",
+  authDomain: "ban-of-storm.firebaseapp.com",
+  projectId: "ban-of-storm",
+  storageBucket: "ban-of-storm.appspot.com",
+  messagingSenderId: "999067934263",
+  appId: "1:999067934263:web:b6c32c28a7fa3a85c6a9b2",
+};
+
+var arrImgHeroes;
+var arrUrlHeroes = {};
+var jsonUrlBlackList;
+
+const app = initializeApp(firebaseConfig);
+
+// Get type FirebaseStorage
+const storage = getStorage(app, app.storageBucket);
+
+// Get type StorageReference
+const storageRef = ref(storage, "img");
+const jsonRef = ref(storage, "json");
+// const storageRef = ref(storage, "img/Abathur.png");
+
+export function fetchUrlsImg() {
+  return new Promise((resolve, reject) => {
+    listAll(storageRef).then((r) => {
+      arrImgHeroes = r.items.map((item) => {
+        const key = item.name.split(".")[0];
+        const value = item._location.path_;
+        return { [key]: value };
+      });
+
+      // Download Promises
+      let promises = arrImgHeroes.map((obj) => {
+        const [key] = Object.keys(obj);
+        const path = obj[key];
+
+        return getDownloadURL(ref(storage, path)).then((url) => {
+          arrUrlHeroes[key] = url;
+        });
+      });
+
+      promises.push(
+        getDownloadURL(ref(jsonRef, "blacklist.json")).then((url) => {
+          jsonUrlBlackList = url;
+        })
+      );
+
+      // Wait for all Promises DownloadURL
+      Promise.all(promises)
+        .then(() => {
+          resolve([arrUrlHeroes, jsonUrlBlackList]);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  });
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+import { fetchUrlsImg } from "./config.js";
+
 var playersHTML = "";
 
 const heroLevel = {
@@ -36,11 +38,26 @@ document.getElementById("inputSearch").addEventListener("input", function () {
   });
 });
 
-fetch("content/json/blacklist.json")
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((player) => {
-      playersHTML += `
+fetchUrlsImg()
+  .then((r) => {
+    console.log(r);
+    generateHtml(r[0], r[1]);
+  })
+  .catch((error) => {
+    console.error("Error al obtener las URLs de las imágenes:", error);
+  });
+
+function generateHtml(hero_url, json_url) {
+  fetch(json_url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((player) => {
+        playersHTML += `
       <div class="card card-body flex-row gap-3 my-3">
               <div class="card-player-info w-25">
                 <h2>${player.player_name}</h2>
@@ -48,32 +65,33 @@ fetch("content/json/blacklist.json")
               </div>
               <div class="card-heroes-info w-75 align-self-center">
                 <div class="hero-container w-auto">`;
-      player.heroes.forEach((hero) => {
-        playersHTML += `
+        player.heroes.forEach((hero) => {
+          playersHTML += `
           <button type="button" class="btn w-auto rounded-circle btn-${
             heroLevel[hero.level]
           } m-1" data-bs-toggle="popover"
             data-bs-title="${hero.name}"
             data-bs-content="${hero.pop_description}" style="padding: 2px;">
-            <img class="rounded-circle" width="55px" src="content/img/${
-              hero.name
-            }.png" alt="">
+            <img class="rounded-circle" width="55px" src="${
+              hero_url[hero.name]
+            }" alt="">
           </button>`;
-      });
+        });
 
-      playersHTML += `
+        playersHTML += `
                 </div>
               </div>
             </div>`;
 
-      document.querySelector(".backlist-list").innerHTML = playersHTML;
+        document.querySelector(".backlist-list").innerHTML = playersHTML;
 
-      const popoverTriggerList = document.querySelectorAll(
-        '[data-bs-toggle="popover"]'
-      );
-      const popoverList = [...popoverTriggerList].map(
-        (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
-      );
-    });
-  })
-  .catch((error) => console.error("Error al leer el JSON:", error));
+        const popoverTriggerList = document.querySelectorAll(
+          '[data-bs-toggle="popover"]'
+        );
+        const popoverList = [...popoverTriggerList].map(
+          (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+        );
+      });
+    })
+    .catch((error) => console.error("Error al leer el JSON:", error));
+}
